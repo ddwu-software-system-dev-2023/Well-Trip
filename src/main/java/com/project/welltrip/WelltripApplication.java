c6158b401970266f6cc9c0c8efdb9cb24f5647cb
yeon2lee <md04254@naver.com>
2023-05-10 23:12:47 +0900

    [ADD] initial setup

---
+++ src/main/java/com/project/welltrip/WelltripApplication.java
@@ -0,0 +1,13 @@
+package com.project.welltrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelltripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelltripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/welltrip/WelltripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.welltrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelltripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
