5611230ef4eac4498e1b6eac7562666cf6e1bc7a
rosie98 <106025529+rosieisor@users.noreply.github.com>
2023-05-28 19:23:04 +0900

    [ADD] Follow Entity

--- src/main/java/com/project/welltrip/domain/Follow.java
+++ src/main/java/com/project/welltrip/domain/Follow.java
@@ -1,4 +1,41 @@
 package com.project.welltrip.domain;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Data
+@Setter
+@Getter
+@Entity
+@SequenceGenerator(
+        name = "FOLLOW_SEQ_GENERATOR",
+        sequenceName = "FOLLOW_SEQ", // 시퀸스 명
+        allocationSize = 1 // 미리 할당 받을 시퀸스 수
+)
+@Table(name = "follow")
 public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FOLLOW_SEQ_GENERATOR")
+    private Long followId; // follow Id
+
+    private String follower; // 로그인한 사용자
+
+    @NotEmpty
+    private String followee; // 친구 신청을 당하는 사용자
+
+    @NotEmpty
+    private LocalDateTime followDate;
+
+    public Follow() {
+    }
+
+    public Follow(Long followId) {
+        this.followId = followId;
+    }
 }
